b4d7850640fabfcb0435ec2dfa06373df12e467c
Lucas lima <lucasliman4@gmail.com>
2026-04-04 15:59:13 -0300

    adicionamos o node.js

---
+++ public/javascripts/mudartema.js
@@ -0,0 +1,22 @@
+function alternarTema(){
+    document.body.classList.toggle("claro")
+
+    const botao = document.getElementById("toggleTema")
+
+    if(document.body.classList.contains("claro")){
+        botao.textContent = "🌕"
+    }else{
+        botao.textContent = "🌑"
+    }
+}
+document.addEventListener("DOMContentLoaded",function(){
+
+    const botao = document.getElementById("toggleTema")
+
+    if(document.body.classList.contains("claro")){
+        botao.textContent = "🌕"
+    }else{
+        botao.textContent = "🌑"
+    }
+
+})
